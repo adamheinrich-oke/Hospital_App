15a966094821e2e5028c74e24e06a9f071bbb282
adamheinrich <adam.heinrich@oke.pl>
2022-05-08 18:45:25 +0200

    nurse 2.0

---
+++ app/src/main/java/com/example/ahzd/nurseactivity/nursefragment/AddPatient.kt
@@ -0,0 +1,9 @@
+package com.example.ahzd.nurseactivity.nursefragment
+
+import androidx.fragment.app.Fragment
+import com.example.ahzd.R
+
+class AddPatient() : Fragment(R.layout.addpatient) {
+
+
+    }
